6887e925b23f1baf053d5b0a10da795184365aa7
boriskisiov <boriskisiov@gmail.com>
2023-10-03 15:33:32 +0300

    Add project files.

---
+++ Food or Drink/Program.cs
@@ -0,0 +1,12 @@
+﻿string pr = Console.ReadLine();
+
+if (pr == "curry" || pr == "noodels" || pr == "sushi" || pr == "spagetti" || pr == "bread")
+{
+    Console.WriteLine("Food");
+}
+else if (pr == "tea" || pr == "water" || pr == "coffe" || pr == "juice")
+{
+    Console.WriteLine("Drink");
+}
+else
+    Console.WriteLine("unknown");
